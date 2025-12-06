493479f9fbc391573ab6b065db5ca51a205667c0
Lightromax-Jetlearn <calendara132@gmail.com>
2025-12-06 16:34:55 +0000

    Add files via upload

--- stopwatch.py
+++ stopwatch.py
@@ -7,6 +7,10 @@ root.geometry("600x300")
 ###############################################
 #define
 def start():
+    start_button.config(state="disabled")
+    Hour_box.config(state="disabled")
+    Minute_box.config(state="disabled")
+    second_box.config(state="disabled")
     try:
         temp= int(Hour_box.get())*3600 + int(Minute_box.get())*60 + int(second_box.get())
     except:
@@ -16,6 +20,14 @@ def start():
         hours=00
         if mins > 60:
             hours,mins=divmod(mins,60)
+        hour.set("{00:2d}".format(hours))
+        minute.set("{00:2d}".format(mins))
+        second.set("{00:2d}".format(secs))
+        root.update()
+        sleep(1)
+        if temp == 00:
+            showinfo("countdown","TIMES UPPP!!!!!!!")
+        temp -= 1
 ###############################################
 #variables
 hour=StringVar()
@@ -30,7 +42,7 @@ second.set("00")
 Hour_box=Entry(root,width=11,textvariable=hour,font=("Ariel",18,""))
 Minute_box=Entry(root,width=11,textvariable=minute,font=("Ariel",18,""))
 second_box=Entry(root,width=11,textvariable=second,font=("Ariel",18,""))
-start_button=Button(root,text="Start Timer")
+start_button=Button(root,text="Start Timer",command=start)
 ###############################################
 #Placing
 Hour_box.place(x=200,y=100)
