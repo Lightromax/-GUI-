d5f498d05c0cf522b68f534d47c2014803d0b2d3
Lightromax-Jetlearn <calendara132@gmail.com>
2025-12-27 16:39:11 +0000

    Add files via upload

--- homework12.py
+++ homework12.py
@@ -8,12 +8,23 @@ root.geometry("500x500")
 r=random.randint(0,255)
 g=random.randint(0,255)
 b=random.randint(0,255)
+
+#def addcolor():
+
+def changebg(event):
+    try:
+        selectedcolor=listbox.get(listbox.curselection())
+        root.config(background=selectedcolor)
+    except:
+        pass
 frame=Frame(root,width=45,height=45)
 scrollbar=Scrollbar(frame)
 listbox=Listbox(frame,yscrollcommand=scrollbar.set)
-for i in range(101):
-    listbox.insert(END,str(i))
 
+colors=["red","blue","yellow","green","purple"]
+for c in colors:
+    listbox.insert(END,c)
+listbox.bind("<<ListboxSelect>>",changebg)
 frame.pack(anchor=CENTER)
 scrollbar.pack(anchor=CENTER,side=RIGHT,fill=Y)
 listbox.pack(anchor=CENTER,side=RIGHT)
