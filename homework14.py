7fbd1d7a132145078f2d1c2b5cadd9e5250525b0
Lightromax-Jetlearn <calendara132@gmail.com>
2026-01-17 16:20:06 +0000

    Add files via upload

---
+++ homework14.py
@@ -0,0 +1,38 @@
+from tkinter import *
+from tkinter.ttk import *
+
+root=Tk()
+root.geometry("500x500")
+root.title("Student Grading System")
+
+Title_label=Label(root,text="Student Grading System",font=("Ariel",18))
+name_label=Label(root,text="Name:",font=("Ariel",10))
+name_box=Entry(root)
+roll_label=Label(root,text="Roll no:",font=("Ariel",10))
+roll_box=Entry(root)
+math_label=Label(root,text="Math Marks:",font=("Ariel",10))
+math_box=Entry(root)
+science_label=Label(root,text="Science Marks:",font=("Ariel",10))
+science_box=Entry(root)
+english_label=Label(root,text="English Marks:",font=("Ariel",10))
+english_box=Entry(root)
+cal_button=Button(root,text="Calculate Grade")
+save_button=Button(root,text="Save Grade")
+listbox=Listbox(root,width=30)
+
+Title_label.place(x=10,y=10)
+name_label.place(x=20,y=50)
+name_box.place(x=120,y=50)
+roll_label.place(x=20,y=70)
+roll_box.place(x=120,y=70)
+math_label.place(x=20,y=90)
+math_box.place(x=120,y=90)
+science_label.place(x=20,y=110)
+science_box.place(x=120,y=110)
+english_label.place(x=20,y=130)
+english_box.place(x=120,y=130)
+cal_button.place(x=50,y=160)
+save_button.place(x=57,y=190)
+listbox.place(x=10,y=240)
+
+root.mainloop()
